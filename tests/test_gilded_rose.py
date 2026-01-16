05c3d015e311794ff2da37889a8f293c9b4aa49d
VulturARG <luisbriones@gmail.com>
2026-01-16 09:50:40 -0300

    Exploit Item to file and change to dataclass.

--- tests/test_gilded_rose.py
+++ tests/test_gilded_rose.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from unittest import TestCase
 
-from gilded_rose import Item, GildedRose
+from gilded_rose import GildedRose
+from item import Item
 
 
 class GildedRoseTest(TestCase):
--- texttest_fixture.py
+++ texttest_fixture.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
-from gilded_rose import *
+from gilded_rose import GildedRose
+from item import Item
 
 
 def main():
@@ -19,6 +20,7 @@ def main():
     ]
     days = 2
     import sys
+
     if len(sys.argv) > 1:
         days = int(sys.argv[1]) + 1
     for day in range(days):
